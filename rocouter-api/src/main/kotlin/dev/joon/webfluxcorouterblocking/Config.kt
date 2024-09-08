bcbff86ac9fed4bbfda8db56033c1ad0345528fa
sijun-yang <yangsijun@bondit.io>
2024-09-08 21:16:51 +0900

    Update Config.kt

--- rocouter-api/src/main/kotlin/dev/joon/webfluxcorouterblocking/Config.kt
+++ rocouter-api/src/main/kotlin/dev/joon/webfluxcorouterblocking/Config.kt
@@ -3,6 +3,7 @@ package dev.joon.webfluxcorouterblocking
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.http.MediaType
@@ -10,16 +11,16 @@ import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.coRouter
 import org.springframework.web.reactive.function.server.bodyValueAndAwait
 
-@Bean
-fun apiRouter(
-    repository: Repository
-) = coRouter {
-    accept(MediaType.APPLICATION_JSON).nest {
-        GET("/ask") {
-            ServerResponse.ok().bodyValueAndAwait {
-                repository.callDb()
-                mapOf("message" to "HI!")
-            }
+@Configuration
+class Config {
+    @Bean
+    fun apiRouter(
+        repository: Repository
+    ) = coRouter {
+        GET("/ask") { request ->
+            repository.callDb()
+            val response = mapOf("message" to "HI!")
+            ServerResponse.ok().bodyValueAndAwait(response)
         }
     }
 }
